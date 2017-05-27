412de84e3ed50dbacf7bd33aad84e8db63881049
shizzz477 <marcgurreri@gmail.com>
2017-05-27 21:13:17 +0900

    Auto stash before merge of "master" and "origin/master"

---
+++ bin/pilconvert.py
@@ -0,0 +1,4 @@
+#!/mnt/HDDATA/src/MITMf/bin/python
+# EASY-INSTALL-SCRIPT: 'Pillow==4.1.1','pilconvert.py'
+__requires__ = 'Pillow==4.1.1'
+__import__('pkg_resources').run_script('Pillow==4.1.1', 'pilconvert.py')
